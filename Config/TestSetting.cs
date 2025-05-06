37e4eb01157ee77d606a7f1ed42f40140961a562
Aleksandra Tolmachova <a.f.tolmacheva@gmail.com>
2025-05-06 14:50:45 +0300

    universal methods

--- Config/TestSetting.cs
+++
@@ -1,9 +0,0 @@
-namespace Gobigreviews.Config
-{
-    public static class TestSettings
-    {
-        public static string BaseUrl =>"https://gobigreviews.com/";
-        public static int DefaultTimeOut => 10;
-        
-    }
-}
